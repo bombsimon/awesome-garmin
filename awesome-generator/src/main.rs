a2d46d351b6da320e2612b15a58b959c6aae1669
Simon Sawert <simon@sawert.se>
2024-03-07 00:01:17 +0100

    Improve handling of each item
    * Use separate structs to represent our `awesome.toml` and the output in
      our template
    * Add resource type enum to be able to match on type
    * Split active and inactive items already when fetching to avoid double
      iteration
    * Write directly to `BTReeMap` to avoid intermediate vectors
    * Skip items that can't be fetched or doesn't have any name
    * Fix error in template never showing inactive widgets
    * Fix typos

--- awesome-generator/src/main.rs
+++ awesome-generator/src/main.rs
@@ -6,7 +6,7 @@ use serde_with::{serde_as, NoneAsEmptyString};
 use std::{
     collections::{BTreeMap, HashMap},
     io::Read,
-    sync::Arc,
+    sync::{Arc, Mutex},
 };
 
 const TEMPLATE: &str = include_str!("readme.md.hbs");
@@ -19,45 +19,78 @@ const MAX_CONCURRENT_REQUESTS: usize = 20;
 /// might still be useful for reference but are put away under a separate menu to reduce noise.
 const MAX_AGE_BEFORE_OLD: std::time::Duration = std::time::Duration::from_secs(86400 * 365 * 2);
 
+#[derive(Clone)]
+enum ResourceType {
+    WatchFace,
+    DataField,
+    Widget,
+    DeviceApp,
+    AudioContentProvider,
+    Barrel,
+    Tool,
+    CompanionApp,
+    Miscellaneous,
+}
+
+impl ResourceType {
+    fn map_key(&self, is_old: bool) -> String {
+        let key = match self {
+            Self::WatchFace => "watch_face",
+            ResourceType::DataField => "data_field",
+            ResourceType::Widget => "widget",
+            ResourceType::DeviceApp => "device_app",
+            ResourceType::AudioContentProvider => return "audio_content_provider".to_string(),
+            ResourceType::Barrel => return "barrel".to_string(),
+            ResourceType::Tool => return "tool".to_string(),
+            ResourceType::CompanionApp => return "companion_app".to_string(),
+            ResourceType::Miscellaneous => return "miscellaneous".to_string(),
+        };
+
+        if is_old {
+            format!("{}_inactive", key)
+        } else {
+            format!("{}_active", key)
+        }
+    }
+}
+
 #[serde_as]
 #[derive(Debug, Serialize, Deserialize)]
-struct Resource {
+struct TomlFileItem {
     #[serde_as(as = "NoneAsEmptyString")]
     #[serde(default)]
     name: Option<String>,
     #[serde_as(as = "NoneAsEmptyString")]
     #[serde(default)]
     description: Option<String>,
-    #[serde(skip_deserializing)]
-    url: String,
-    #[serde(skip_deserializing)]
-    owner: Option<String>,
-    #[serde(skip_deserializing)]
-    repo: Option<String>,
-    #[serde(skip_deserializing, with = "ymd_date")]
-    last_updated: Option<chrono::DateTime<chrono::Utc>>,
-    #[serde(default)]
-    is_archived: bool,
-    #[serde(default)]
-    is_old: bool,
 }
 
 #[derive(Debug, Deserialize)]
-struct Resources {
-    watch_faces: HashMap<String, Resource>,
-    data_fields: HashMap<String, Resource>,
-    widgets: HashMap<String, Resource>,
-    device_apps: HashMap<String, Resource>,
-    audio_content_providers: HashMap<String, Resource>,
-    barrels: HashMap<String, Resource>,
-    tools: HashMap<String, Resource>,
-    companion_apps: HashMap<String, Resource>,
-    miscellaneous: HashMap<String, Resource>,
+struct TomlFile {
+    watch_faces: HashMap<String, TomlFileItem>,
+    data_fields: HashMap<String, TomlFileItem>,
+    widgets: HashMap<String, TomlFileItem>,
+    device_apps: HashMap<String, TomlFileItem>,
+    audio_content_providers: HashMap<String, TomlFileItem>,
+    barrels: HashMap<String, TomlFileItem>,
+    tools: HashMap<String, TomlFileItem>,
+    companion_apps: HashMap<String, TomlFileItem>,
+    miscellaneous: HashMap<String, TomlFileItem>,
+}
+
+#[derive(Debug, Serialize)]
+struct GarminResource {
+    name: String,
+    description: Option<String>,
+    url: String,
+    #[serde(with = "ymd_date")]
+    last_updated: Option<chrono::DateTime<chrono::Utc>>,
+    is_archived: bool,
 }
 
 #[derive(Serialize)]
-struct Template<'a> {
-    resources: BTreeMap<&'a str, Vec<Resource>>,
+struct Template {
+    resources: BTreeMap<String, Vec<GarminResource>>,
     updated_at: String,
 }
 
@@ -69,7 +102,7 @@ async fn main() -> Result<(), &'static str> {
         .read_to_string(&mut toml_content)
         .expect("Failed to read awesome.toml");
 
-    let mut resources: Resources = toml::from_str(&toml_content).expect("Failed to parse TOML");
+    let resources: TomlFile = toml::from_str(&toml_content).expect("Failed to parse TOML");
     let octocrab = Arc::new(
         octocrab::OctocrabBuilder::new()
             .personal_token(std::env::var("GITHUB_TOKEN").unwrap())
@@ -83,30 +116,57 @@ async fn main() -> Result<(), &'static str> {
             .unwrap(),
     );
 
+    let data: Arc<Mutex<BTreeMap<String, Vec<GarminResource>>>> =
+        Arc::new(Mutex::new(BTreeMap::new()));
+
     let mut futures = Vec::new();
-    for resources in [
-        &mut resources.watch_faces,
-        &mut resources.data_fields,
-        &mut resources.widgets,
-        &mut resources.device_apps,
-        &mut resources.audio_content_providers,
-        &mut resources.barrels,
-        &mut resources.tools,
-        &mut resources.companion_apps,
-        &mut resources.miscellaneous,
+    for (resource_type, resources) in [
+        (ResourceType::WatchFace, resources.watch_faces),
+        (ResourceType::DataField, resources.data_fields),
+        (ResourceType::Widget, resources.widgets),
+        (ResourceType::DeviceApp, resources.device_apps),
+        (
+            ResourceType::AudioContentProvider,
+            resources.audio_content_providers,
+        ),
+        (ResourceType::Barrel, resources.barrels),
+        (ResourceType::Tool, resources.tools),
+        (ResourceType::CompanionApp, resources.companion_apps),
+        (ResourceType::Miscellaneous, resources.miscellaneous),
     ] {
-        for (resource_url, resource) in resources.iter_mut() {
+        for (resource_url, resource) in resources {
+            let rt = resource_type.clone();
             let oc = octocrab.clone();
             let gl = glab.clone();
+            let d = data.clone();
 
             futures.push(async move {
-                resource.url = resource_url.to_string();
-                eprintln!("Updating {}", resource.url);
+                eprintln!("Updating {}", resource_url);
 
-                if resource_url.contains("github.com") {
-                    update_github_resource(resource, oc).await;
+                let (resource, is_old) = if resource_url.contains("github.com") {
+                    update_github_resource(resource_url, &resource, oc).await
                 } else if resource_url.contains("gitlab.com") {
-                    update_gitlab_resource(resource, gl).await;
+                    update_gitlab_resource(resource_url, &resource, gl).await
+                } else if let Some(name) = resource.name {
+                    (
+                        Some(GarminResource {
+                            name,
+                            description: resource.description,
+                            url: resource_url,
+                            last_updated: None,
+                            is_archived: false,
+                        }),
+                        true,
+                    )
+                } else {
+                    return;
+                };
+
+                if let Some(resource) = resource {
+                    let key = rt.map_key(is_old);
+                    let mut m = d.lock().unwrap();
+                    let elem = m.entry(key).or_default();
+                    elem.push(resource)
                 }
             });
         }
@@ -115,26 +175,19 @@ async fn main() -> Result<(), &'static str> {
     let stream = futures::stream::iter(futures).buffer_unordered(MAX_CONCURRENT_REQUESTS);
     stream.collect::<Vec<_>>().await;
 
-    let mut data = BTreeMap::new();
-    data.insert("watch_face", sorted_resources(resources.watch_faces));
-    data.insert("data_field", sorted_resources(resources.data_fields));
-    data.insert("widget", sorted_resources(resources.widgets));
-    data.insert("device_app", sorted_resources(resources.device_apps));
-    data.insert(
-        "audio_content_provider",
-        sorted_resources(resources.audio_content_providers),
-    );
-    data.insert("barrel", sorted_resources(resources.barrels));
-    data.insert("tool", sorted_resources(resources.tools));
-    data.insert("companion_app", sorted_resources(resources.companion_apps));
-    data.insert("miscellaneous", sorted_resources(resources.miscellaneous));
-
     let mut hb = Handlebars::new();
     hb.register_escape_fn(no_escape);
     hb.register_template_string("readme", TEMPLATE).unwrap();
 
+    {
+        let mut d = data.lock().unwrap();
+        for (_, v) in d.iter_mut() {
+            sorted_resources(v);
+        }
+    }
+
     let template = Template {
-        resources: data,
+        resources: Arc::try_unwrap(data).unwrap().into_inner().unwrap(),
         updated_at: chrono::Utc::now().format("%Y-%m-%d").to_string(),
     };
 
@@ -143,51 +196,60 @@ async fn main() -> Result<(), &'static str> {
     Ok(())
 }
 
-fn sorted_resources(resources: HashMap<String, Resource>) -> Vec<Resource> {
-    let mut r = resources.into_values().collect::<Vec<_>>();
-
-    r.sort_by(|a, b| match (a.last_updated, b.last_updated) {
-        (None, None) => match (&a.name, &b.name) {
-            (Some(a), Some(b)) => a.cmp(b),
-            _ => std::cmp::Ordering::Equal,
-        },
+fn sorted_resources(resources: &mut [GarminResource]) {
+    resources.sort_by(|a, b| match (a.last_updated, b.last_updated) {
+        (None, None) => a.name.cmp(&b.name),
         (Some(a), Some(b)) => b.cmp(&a),
         (None, Some(_)) => std::cmp::Ordering::Greater,
         (Some(_), None) => std::cmp::Ordering::Less,
     });
-
-    r
 }
 
-async fn update_github_resource(resource: &mut Resource, octocrab: Arc<octocrab::Octocrab>) {
-    let u = url::Url::parse(&resource.url).unwrap();
+async fn update_github_resource(
+    resource_url: String,
+    resource: &TomlFileItem,
+    octocrab: Arc<octocrab::Octocrab>,
+) -> (Option<GarminResource>, bool) {
+    let u = url::Url::parse(&resource_url).unwrap();
     let mut owner_repo = u.path().strip_prefix('/').unwrap().split('/');
     let owner = owner_repo.next().unwrap();
     let repo = owner_repo.next().unwrap();
     let result = match octocrab.repos(owner, repo).get().await {
         Ok(result) => result,
         Err(err) => {
-            eprintln!("⚠️ Could not get {}: {err}", resource.url);
-            return;
+            eprintln!("⚠️ Could not get {}: {err}", resource_url);
+            return (None, false);
         }
     };
 
-    if resource.description.is_none() {
-        resource.description = result.description;
-    }
+    let garmin_resource = GarminResource {
+        name: repo.to_string(),
+        description: Some(
+            resource
+                .description
+                .clone()
+                .unwrap_or(result.description.unwrap_or_default()),
+        ),
+        url: resource_url.to_string(),
+        last_updated: result.pushed_at,
+        is_archived: result.archived.unwrap_or_default(),
+    };
 
-    resource.owner = Some(owner.to_string());
-    resource.repo = Some(repo.to_string());
-    resource.last_updated = result.pushed_at;
-    resource.is_archived = result.archived.unwrap_or_default();
+    let is_old = if let Some(pushed_at) = result.pushed_at {
+        pushed_at < chrono::Utc::now() - MAX_AGE_BEFORE_OLD
+    } else {
+        false
+    };
 
-    if let Some(pushed_at) = result.pushed_at {
-        resource.is_old = pushed_at < chrono::Utc::now() - MAX_AGE_BEFORE_OLD;
-    }
+    (Some(garmin_resource), is_old)
 }
 
-async fn update_gitlab_resource(resource: &mut Resource, glab: Arc<gitlab::AsyncGitlab>) {
-    let u = url::Url::parse(&resource.url).unwrap();
+async fn update_gitlab_resource(
+    resource_url: String,
+    resource: &TomlFileItem,
+    glab: Arc<gitlab::AsyncGitlab>,
+) -> (Option<GarminResource>, bool) {
+    let u = url::Url::parse(&resource_url).unwrap();
     let owner_repo = u.path().strip_prefix('/').unwrap();
     let endpoint = gitlab::api::projects::Project::builder()
         .project(owner_repo)
@@ -196,20 +258,28 @@ async fn update_gitlab_resource(resource: &mut Resource, glab: Arc<gitlab::Async
     let result: gitlab::Project = match endpoint.query_async(&*glab).await {
         Ok(result) => result,
         Err(err) => {
-            eprintln!("⚠️ Could not get {}: {err}", resource.url);
-            return;
+            eprintln!("⚠️ Could not get {}: {err}", resource_url);
+            return (None, false);
         }
     };
 
-    if resource.description.is_none() {
-        resource.description = result.description;
-    }
+    let garmin_resource = GarminResource {
+        name: result.name,
+        description: Some(
+            resource
+                .description
+                .clone()
+                .unwrap_or(result.description.unwrap_or_default()),
+        ),
+        url: resource_url.to_string(),
+        last_updated: Some(result.last_activity_at),
+        is_archived: result.archived,
+    };
 
-    resource.owner = Some(result.namespace.path);
-    resource.repo = Some(result.name);
-    resource.last_updated = Some(result.last_activity_at);
-    resource.is_archived = result.archived;
-    resource.is_old = result.last_activity_at < chrono::Utc::now() - MAX_AGE_BEFORE_OLD;
+    (
+        Some(garmin_resource),
+        result.last_activity_at < chrono::Utc::now() - MAX_AGE_BEFORE_OLD,
+    )
 }
 
 mod ymd_date {
